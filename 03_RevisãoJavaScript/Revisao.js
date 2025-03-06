4da93d1932bfa0ba0f5261cfd9c33fb017a185e1
Yassanalitics <yasminribeiro152005@gmail.com>
2025-03-05 21:21:38 -0300

    Aula

---
+++ 03_RevisãoJavaScript/Revisao.js
@@ -0,0 +1,24 @@
+console.log("testeee")
+//comentário de uma linha 
+/*cometario 
+de varias 
+linhas*/
+var nome = "yasmin"; //escopo global 
+let  idade = 19; //escopo de bloco 
+const cidade = Brasília; //nao pode ser reatribuida
+//primitivos 
+let Nome = "Gustavo"; // string
+let Idade = 25; // number
+let ativo = true; // boolean
+let vazio = null; // null
+let indefinido; // undefined
+
+console.log (Nome)
+let iidade = 18;
+if (iidade < 18) {
+console.log("Menor de idade");
+} else if (iidade === 18) {
+console.log("Tem 18 anos");
+} else {
+console.log("Maior de idade");
+}
