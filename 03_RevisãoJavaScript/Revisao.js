80089aca79ae1e0b69e35862589bdcc26037e8fa
Yassanalitics <yasminribeiro152005@gmail.com>
2025-03-06 23:50:47 -0300

    Aula06/03

--- 03_RevisãoJavaScript/Revisao.js
+++ 03_RevisãoJavaScript/Revisao.js
@@ -5,7 +5,7 @@ de varias
 linhas*/
 var nome = "yasmin"; //escopo global 
 let  idade = 19; //escopo de bloco 
-const cidade = Brasília; //nao pode ser reatribuida
+
 //primitivos 
 let Nome = "Gustavo"; // string
 let Idade = 25; // number
@@ -22,3 +22,43 @@ console.log("Tem 18 anos");
 } else {
 console.log("Maior de idade");
 }
+
+//switch case 
+let dia = 3; 
+switch (dia) { 
+case 1: 
+console.log("Domingo"); 
+break; 
+case 2: 
+console.log("Segunda-feira"); 
+break; 
+case 3: 
+console.log("Terça-feira"); 
+break; 
+default: 
+console.log("Dia inválido"); 
+} 
+
+//objetos
+let pessoa = {
+    nome:  "yasmin",
+    idade: 20,
+    curso: "ADS"
+
+}
+console.log(pessoa.nome)
+
+//function 
+function soma(num1, num2) {
+    const result = num1 + num2
+    console.log(result)
+    
+}
+soma(3,5)
+soma(10,10)
+
+//modulos
+import {calculadoraIMC}from "./calculoimc"
+const res = calculoimc.calculadoraIMC(80, 70 )
+
+console.log(res)
