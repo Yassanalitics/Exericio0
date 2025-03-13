0318ec0e9ccbb1484571ab9355f1e5b3ef6ab7d9
Yassanalitcs <yasminribeiro152005@gmail.com>
2025-03-12 21:27:25 -0300

    exercicio aula 4

---
+++ 04- npm/calculadora-projeto/calculadora.js
@@ -0,0 +1,13 @@
+export function adicao(n1, n2){
+    return n1 + n2
+}
+export function subtracao(n1, n2 ){
+    return n1 - n2 
+}
+export function multiplicacao(n1, n2){
+    return n1 * n2 
+}
+export function divisao(n1, n2){
+    return n1/n2 
+}
+
